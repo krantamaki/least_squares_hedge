8482a19638e8e43cb07395f4def7461313be635c
krantamaki <kasper.rantamaki@gmail.com>
2025-10-07 17:51:13 +0300

    Removing old directories

--- least_squares_hedge/__init__.py
+++
@@ -1,8 +0,0 @@
-"""@package least_squares_hedge
-@author Kasper Rantamäki
-Module for the least squares hedge strategy
-"""
-
-__all__ = ["PathIndependentLeastSquaresHedge"]
-
-from PathIndependentLeastSquaresHedge import PathIndependentLeastSquaresHedge
